db845ac6bdd10853b4c3f3fde0ae2c9b6f9e3085
Kaushal <107054063+penguin-404@users.noreply.github.com>
2023-12-29 09:18:30 +0000

    datatypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all JS codes as newer version.
+
+//alert(3+1) //using nodejs and not using browser.
+
+console.log(1
+    +2) //executes successfully but reduces code redability. So, it is a bad practice.
+
+let name = "Ram"
+let age = 18
+let isLoggedIn = true
+
+//Data Types in JS
+//number
+//bigint
+//string => use double quotations ""
+//boolean => true/false
+//null => standalone value
+//undefined
+//symbol => unique
+//object
+//typeof
+
+console.log(typeof age)
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined

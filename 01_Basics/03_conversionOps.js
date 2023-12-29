77fcfc95e4268240a930efd252f23df1b2879776
Kaushal <107054063+penguin-404@users.noreply.github.com>
2023-12-29 12:14:45 +0000

    Operations

--- 01_Basics/03_conversionOps.js
+++ 01_Basics/03_conversionOps.js
@@ -1,10 +1,10 @@
 let score = "12"
 
-console.log(typeof score)
+// console.log(typeof score)
 
 let vauleInNumber = Number(score)
-console.log(typeof vauleInNumber)
-console.log(vauleInNumber);
+// console.log(typeof vauleInNumber)
+// console.log(vauleInNumber);
 
 // Conversion Results
 // "12" => 12
@@ -15,12 +15,37 @@ console.log(vauleInNumber);
 
 let isLoggedIn = 8
 let BoolLoggedIn = Boolean(isLoggedIn)
-console.log(BoolLoggedIn);
+// console.log(BoolLoggedIn);
 
 // 1 => true; 0 => false
 // "" => false; "abc" => true
 
 let someNumber = 12
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// Operations
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**4);
+// console.log(2/4);
+// console.log(2%4);
+
+let str1 = "Hello"
+let str2 = " Developers"
+let str3 = str1 + str2 
+// console.log(str3);
+
+// console.log(1 + "2");
+// console.log("1" + 2);
+// console.log("1" + 2 + 3);
+// console.log(1 + 2 + "3");
+
+console.log(true);

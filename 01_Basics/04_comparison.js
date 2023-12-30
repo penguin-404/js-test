d8542a881064ec7399e5ab3786b739984bc1a464
Kaushal <107054063+penguin-404@users.noreply.github.com>
2023-12-30 08:16:58 +0000

    Comparisons in js

---
+++ 01_Basics/04_comparison.js
@@ -0,0 +1,16 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<=1);
+// console.log(2!=1);
+
+// console.log("2">3);
+// console.log("02">1);
+
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+
+// Comparisons convert null to a number, treating it as 0. That's why null>=0 is true and null>0 is false.
+
+// strict checks
+console.log("2"===2);

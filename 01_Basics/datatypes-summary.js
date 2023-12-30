c9d0a4e701ff16f286a42bcf77a1f7d3c7df7673
Kaushal <107054063+penguin-404@users.noreply.github.com>
2023-12-30 08:41:07 +0000

    Datatype Summary

---
+++ 01_Basics/datatypes-summary.js
@@ -0,0 +1,19 @@
+// Primitive
+// 7 Types: String,Number,Boolean,BigInt,Null,Undefined,Symbol
+
+//Js is dynamically typed.
+
+const score = 100
+const scoreValue = 100.1
+
+const isLoggedIn = true
+const outsideTemp = null
+let userEmail;
+
+
+const id = Symbol('12')
+const anotherId = Symbol('12')
+console.log(id === anotherId);
+
+// Non-Primitive[Reference]
+// Arrays,Objects,Functions

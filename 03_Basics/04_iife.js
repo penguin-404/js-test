2985abd961d72a1a77baba5644cfc4fce8cd28f6
Kaushal <107054063+penguin-404@users.noreply.github.com>
2024-01-02 15:11:31 +0000

    IIFE in JS

---
+++ 03_Basics/04_iife.js
@@ -0,0 +1,16 @@
+// Immediately Invoked Function Expressions [IIFE]
+
+(function chai(){
+    // named IIFE
+    console.log(`DB Connected`);
+})();
+
+// IIFE => Global scope causes pollution a lot of times in the program. To remove the pollution that are caused due to the declaration of the global variables IIFE is used.
+// Simple IIFE or Unnamed IIFE
+( () => {
+    console.log(`DB Connected two`);
+} )();
+
+( (name) => {
+    console.log(`DB Connected two: ${name}`);
+} )('MySQL')

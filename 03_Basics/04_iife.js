ce80324e7291476cdbfff840f48e7aa499f3bf8b
Kaushal <107054063+penguin-404@users.noreply.github.com>
2024-01-02 15:22:22 +0000

    iife 2

--- 03_Basics/04_iife.js
+++ 03_Basics/04_iife.js
@@ -2,15 +2,15 @@
 
 (function chai(){
     // named IIFE
-    console.log(`DB Connected`);
+    // console.log(`DB Connected`);
 })();
 
 // IIFE => Global scope causes pollution a lot of times in the program. To remove the pollution that are caused due to the declaration of the global variables IIFE is used.
 // Simple IIFE or Unnamed IIFE
 ( () => {
-    console.log(`DB Connected two`);
+    // console.log(`DB Connected two`);
 } )();
 
 ( (name) => {
-    console.log(`DB Connected two: ${name}`);
+    console.log(`She is not just SQL. She is ${name}`);
 } )('MySQL')

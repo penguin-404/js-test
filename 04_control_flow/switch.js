90bd54641de3653387321d8de2097189b97cffad
Kaushal <107054063+penguin-404@users.noreply.github.com>
2024-01-03 13:09:05 +0000

    switch statement in js

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,30 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 1
+
+switch (month) {
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("February");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+        
+
+    default:
+        console.log("Month value is not between 1 and 12");
+        break;
+}

88c21e942778a0030c9033168de7a4135362aa68
Kaushal <107054063+penguin-404@users.noreply.github.com>
2024-02-09 06:04:05 +0000

    BackgroundColorChanger using Js

---
+++ ColorChanger/script.js
@@ -0,0 +1,26 @@
+const buttons = document.querySelectorAll('.button')
+
+const body = document.querySelector("body")
+
+buttons.forEach(function(button){
+    console.log(button);
+    button.addEventListener('click',function(event){
+        switch (event.target.id) {
+            case 'grey':
+                body.style.backgroundColor = event.target.id
+                break;
+            case 'white':
+                body.style.backgroundColor = event.target.id
+                break;
+            case 'blue':
+                body.style.backgroundColor = event.target.id
+                break;
+            case 'yellow':
+                body.style.backgroundColor = event.target.id
+                break;
+            default:
+                body.style.backgroundColor = 'white'
+                break;
+        }
+    })
+})
